1fa0facf4b3443d6473d4fd4e20346a4220fd04e
martinsrs <martins.rs@gmail.com>
2015-02-23 22:00:58 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var app = require('express')();
+var http = require ('http').Server(app);
+var io = require('socket.io')(http);
+var schedule = require('node-schedule');
+
+var rule = new schedule.RecurrenceRule();
+rule.minute = new schedule.Range(0, 59, 0);
+
+schedule.scheduleJob('10,20,30,40,50 * * * * *', function() {
+	var text = 'schedule working - ' + new Date();
+
+    console.log('ran at: ' + new Date());
+    io.emit('chat message', text);
+});
+
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+	console.log('a user conected');
+
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	});
+});
+
+http.listen(3000, function(){
+	console.log('listening on *:3000');
+});
