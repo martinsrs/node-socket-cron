c9d462b01d35ac86ddcd038c28d534a7f318afb2
Eduardo Martins <martins.rs@gmail.com>
2015-04-16 09:49:04 -0300

    firebase listening

--- index.js
+++ index.js
@@ -3,14 +3,29 @@ var http = require ('http').Server(app);
 var io = require('socket.io')(http);
 var schedule = require('node-schedule');
 
+var Firebase = require("firebase");
+
+
+var base = new Firebase("https://ditpocbase.firebaseio.com/");
+
+
+base.child("ticketschedule").on("value", function(snapshot) {
+  console.log(snapshot.val());
+
+  if (!snapshot.val().tickets) {
+  	io.emit('notification', snapshot.val().description);
+  }
+
+});
+
 var rule = new schedule.RecurrenceRule();
 rule.minute = new schedule.Range(0, 59, 0);
 
 schedule.scheduleJob('10,20,30,40,50 * * * * *', function() {
 	var text = 'schedule working - ' + new Date();
 
-    console.log('ran at: ' + new Date());
-    io.emit('chat message', text);
+    //console.log('ran at: ' + new Date());
+    //io.emit('chat message', text);
 });
 
 app.get('/', function (req, res) {
